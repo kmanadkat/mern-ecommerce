1199ae3822f08b5cf6f7b295bd8fe7a2ad63f6cf
kmanadkat <kmanadkat@gmail.com>
2022-08-15 21:08:30 +0530

    Added React Bootstrap, Bootswatch, Font Awesome & Header Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
